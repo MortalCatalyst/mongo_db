8bcfa0f82d1d64d51c750ab49f6c325b7e0ef771
mortalcatalyst <saythjwrenshaw@outlook.com>
2017-06-27 22:30:47 +1000

    first commit

---
+++ mongo.py
@@ -0,0 +1,18 @@
+import json
+import pymongo
+import requests
+from pymongo import MongoClient
+
+
+r = requests.get("https://api.tatts.com/sales/vmax/web/data/racing/2017/06/24/sr/full")
+
+# print(r.text)
+client = MongoClient('localhost', 27017)
+db = client.race_dump_database
+collection = db.ubet_api
+
+result = db.result
+result_id = result.insert_one(r.json()).inserted_id
+result_id
+
+print(db.collection_names(include_system_collections=False))
