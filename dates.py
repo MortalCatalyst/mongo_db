4a839d61d0696621ebd17b51171759eb7cf14722
mortalcatalyst <saythjwrenshaw@outlook.com>
2017-07-03 22:11:01 +1000

    formatting

--- dates.py
+++ dates.py
@@ -1,24 +1,28 @@
 import datetime
 
+BASE = datetime.datetime(2017, 1, 1)
 
-base = datetime.datetime(2017,1,1)
 
-def datesRange(max, min):
-    date_list = [base - datetime.timedelta(days=x) for x in range(max,min)]
+def dates_range(max_date, min_date):
+    date_list = [
+        BASE - datetime.timedelta(days=x) for x in range(max_date, min_date)
+    ]
     return date_list
 
-dateValues = datesRange(-210,0)
 
-def dateDay(dayToCheck):
+DATE_VALUES = dates_range(-210, 0)
+
+
+def date_day(daytocheck):
     """
     get a date from the generator
     confirm if its saturday.
     """
-    mySaturdays = []
-    for day in dateValues:
-        if day.weekday() == dayToCheck:
-            mySaturdays.append(day)
-    return mySaturdays
+    my_saturdays = []
+    for day in DATE_VALUES:
+        if day.weekday() == daytocheck:
+            my_saturdays.append(day)
+    return my_saturdays
 
 
-listOfDates = dateDay(5)
+LIST_OF_DATES = date_day(5)
